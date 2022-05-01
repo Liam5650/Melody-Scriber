f855bccabaae237a1b03475bfc8a7b8160f6671a
Liam5650 <62405326+Liam5650@users.noreply.github.com>
2022-05-01 13:30:06 -0400

    Implemented importing and displaying the test image

--- Program Working Copy/melody_scriber.py
+++ Program Working Copy/melody_scriber.py
@@ -1,7 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May  1 12:11:55 2022
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io
+from skimage.color import rgb2gray
 
-@author: Liam
-"""
+# Load image
+img = io.imread('./test_images/test_image_1.png')
 
+# Convert the image to a 2D array representing pixels in grayscale from 0-1
+img = rgb2gray(img)
+
+# Display the image
+plt.imshow(img, cmap='gray')
+plt.title("2D Array Representation of the Image in Grayscale")
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.colorbar();
